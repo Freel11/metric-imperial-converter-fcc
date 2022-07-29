9fc2901676c62459aada8127522988c4b4d7ceaa
jade@itnt.co.za <jade@itnt.co.za>
2022-07-29 12:00:56 +0200

    completing the metric imperial conversion

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,25 +1,114 @@
 function ConvertHandler() {
   
   this.getNum = function(input) {
-    let result;
-    
+    const numberPart = input.match(/[.\d\/]+/g) || ['1'];
+
+    if (numberPart.length > 1) {
+      return undefined
+    }
+
+    const nums = numberPart[0].split('/')
+
+    if (nums.length > 2) {
+      return undefined
+    }
+
+    const num1 = nums[0]
+    const num2 = nums[1] || "1"
+
+    if (isNaN(num1) || isNaN(num2)) {
+      return undefined
+    }
+
+    const result = parseFloat(num1) / parseFloat(num2)
+
     return result;
   };
   
   this.getUnit = function(input) {
-    let result;
-    
+    const unitPart = input.match(/[a-zA-Z]+/g)
+    let result
+
+    switch(unitPart[0].toLowerCase()) {
+      case 'gal':
+        result = 'gal'
+        break
+      case 'l':
+        result = 'L'
+        break
+      case 'mi':
+        result = 'mi'
+        break
+      case 'km':
+        result = 'km'
+        break
+      case 'lbs':
+        result = 'lbs'
+        break
+      case 'kg':
+        result = 'kg'
+        break
+      default:
+        return undefined
+    }
+
     return result;
   };
   
   this.getReturnUnit = function(initUnit) {
     let result;
-    
+
+    switch(initUnit) {
+      case 'gal':
+        result = 'L'
+        break
+      case 'L':
+        result = 'gal'
+        break
+      case 'mi':
+        result = 'km'
+        break
+      case 'km':
+        result = 'mi'
+        break
+      case 'lbs':
+        result = 'kg'
+        break
+      case 'kg':
+        result = 'lbs'
+        break
+      default:
+        return 'something is broken'
+    }
+
     return result;
   };
 
   this.spellOutUnit = function(unit) {
     let result;
+
+    switch(unit) {
+      case 'gal':
+        result = 'gallons'
+        break
+      case 'L':
+        result = 'liters'
+        break
+      case 'mi':
+        result = 'miles'
+        break
+      case 'km':
+        result = 'kilometers'
+        break
+      case 'lbs':
+        result = 'pounds'
+        break
+      case 'kg':
+        result = 'kilograms'
+        break
+      default:
+        return 'something is broken'
+    }
     
     return result;
   };
@@ -29,14 +118,34 @@ function ConvertHandler() {
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
     let result;
+
+    switch (initUnit) {
+      case 'gal':
+        result = initNum * galToL
+        break 
+      case 'L':
+        result = initNum / galToL
+        break
+      case 'mi':
+        result = initNum * miToKm
+        break
+      case 'km':
+        result = initNum / miToKm
+        break
+      case 'lbs':
+        result = initNum / lbsToKg
+        break
+      case 'kg':
+        result = initNum * lbsToKg
+      default:
+        return 'something is broken'
+    }
     
-    return result;
+    return result.toFixed(5);
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
-    return result;
+    return `${initNum} ${this.spellOutUnit(initUnit)} converts to ${returnNum} ${this.spellOutUnit(returnUnit)}`;
   };
   
 }
--- routes/api.js
+++ routes/api.js
@@ -7,4 +7,31 @@ module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
 
+  app.route("/api/converter").get((req, res) => {
+    const input = req.query.input
+    const initNum = convertHandler.getNum(input)
+    const initUnit = convertHandler.getUnit(input)
+
+    if (!initNum && !initUnit) {
+      res.send("invalid number and unit")
+    } else if (!initNum) {
+      res.send("invalid number")
+    } else if (!initUnit) {
+      res.send("invalid unit")
+    } 
+
+    const returnUnit = convertHandler.getReturnUnit(initUnit)
+    const returnNum = convertHandler.convert(initNum, initUnit)
+    const string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit)
+
+    res.send({
+      initNum, 
+      initUnit, 
+      returnNum,
+      returnUnit,
+      string
+    })
+
+  })
+
 };
